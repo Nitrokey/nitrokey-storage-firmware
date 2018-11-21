34aa40e298c3613200ffb37dbb36037868fe1810
NKElias <elias@nitrokey.com>
2018-11-21 20:57:18 +0100

    Increase version number

--- src/global.h
+++ src/global.h
@@ -24,7 +24,7 @@
 // #include "portmacro.h" Don't use is here, system crashes
 
 #define VERSION_MAJOR 0 // 255 = debug version
-#define VERSION_MINOR 52    // 0 = development
+#define VERSION_MINOR 53    // 0 = development
 
 #define INTERNAL_VERSION_NR 0
 
