13ac5e2cdf181951da551021e7464c4efccd9e1b
Szczepan Zalega <szczepan@nitrokey.com>
2021-07-29 12:07:08 +0200

    Bump version to v0.56
    Firmware changes:
    - add HMAC control sum to EV AES key, and abort EV unlock operation if
    it does not match;
    - HMAC is generated on the first use of the EV key, and removed on the
    AES key regeneration and factory reset;
    - The returned error is WRONG_PASSWORD for the backward compatibility
    purposes (to be changed in the future);
    - Removed obsolete matrix-input feature, and connected implementation;
    - Introduce more time-rigid memory comparison function where needed;
    - Reintroduce logs collection through debug file written to UV;
    
    Development:
    - Add helpers for the development (build, upload through bootloader and
    test in one step, without debugger);
    - Divide the main Makefile for better readability;
    - Remove obolete code;
    
    Tested with:
    - libnitrokey v3.6-21-g0748451
    on:
    - Linux Fedora 33

--- src/global.h
+++ src/global.h
@@ -24,7 +24,7 @@
 // #include "portmacro.h" Don't use is here, system crashes
 
 #define VERSION_MAJOR 0 // 255 = debug version
-#define VERSION_MINOR 55    // 0 = development
+#define VERSION_MINOR 56    // 0 = development
 
 #define INTERNAL_VERSION_NR 0
 
